77a16dd33ab919de59eb624c3313f0258d933c13
owen <owen1127@naver.com>
2024-11-19 21:05:42 +0900

    error : hydration error 해결
    트러블 내용
    - Hydration failed because the server rendered HTML didn't match the client 에러가 발생. 에러는 서버에서 렌더링된 HTML과 클라이언트에서 렌더링된 HTML이 일치하지 않을 때 발생
    트러블 해결방안
    - suppressHydrationWarning을 사용하여 해결하였다. 이 방법은 React에서 발생하는 Hydration 경고를 억제하는데 사용되고, shadcn 라이브러리에서 권장하는 해결책이었다. 이를 통해 서버와 클라이언트의 렌더링 불일치 경고를 억제할 수 있었다.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from 'next';
 import '@styles/globals.css';
+import Book from '@/components/Home/Book/Book';
+import { ThemeProvider } from 'next-themes';
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -12,8 +14,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <body>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="light"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <Book>{children}</Book>
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
