4c74147c1dde5069da735f32497fdc92d7e107a0
owen <owen1127@naver.com>
2024-11-22 20:01:50 +0900

    feat : hover hook 생성

--- src/components/Question/QuestionInput/QuestionInput.tsx
+++ src/components/Question/QuestionInput/QuestionInput.tsx
@@ -1,8 +1,21 @@
 'use client';
 import { useState } from 'react';
 import style from './QuestionInput.module.css';
+import { useTheme } from 'next-themes';
+import { useHover } from '@/hooks/useHover';
 const QuestionInput = () => {
+  const { theme } = useTheme();
+
   const [text, setText] = useState('');
+  const { isHovered, handleMouseEnter, handleMouseLeave } = useHover();
+
+  const backgroundColor = isHovered
+    ? theme === 'dark'
+      ? 'black'
+      : '#ffcc89'
+    : theme === 'dark'
+      ? '#777'
+      : '#ebebeb';
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setText(e.target.value);
@@ -16,8 +29,15 @@ const QuestionInput = () => {
         placeholder="질문을 입력하세요"
         className={style.input}
       ></input>
-      <button type="submit" form="answerForm" className={style.answerButton}>
-        🙈 결과
+      <button
+        type="submit"
+        form="answerForm"
+        className={style.answerButton}
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+        style={{ backgroundColor: backgroundColor }}
+      >
+        🙈 결 과
       </button>
     </form>
   );
---
+++ src/hooks/useHover.ts
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+
+// useHover 훅 정의
+export const useHover = () => {
+  const [isHovered, setIsHovered] = useState(false);
+
+  const handleMouseEnter = () => setIsHovered(true);
+  const handleMouseLeave = () => setIsHovered(false);
+
+  return {
+    isHovered,
+    handleMouseEnter,
+    handleMouseLeave,
+  };
+};
